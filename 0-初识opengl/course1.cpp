4941cb56ca2bd6795ebeb7b42c23561ca0e01ae8
bangdreammygo <157594194+bangdreammygo@users.noreply.github.com>
2024-09-17 20:58:51 +0800

    第一课，初步上手opengl
    第一课，初步上手opengl

---
+++ 0-初识opengl/course1.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+
+//注册一个回调函数，在窗口大小发生改变时来回调处理
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+//注册键盘输入函数
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    //初始化GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //创建窗口对象
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    //glad初始化
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //视口设置
+    glViewport(0, 0, 800, 600);
+
+
+    //注册绑定回调函数
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    //我们需要在程序中添加一个while循环，我们可以把它称之为渲染循环(Render Loop)，它能在我们让GLFW退出前一直保持运行。
+    while (!glfwWindowShouldClose(window))
+    {
+        //检查键盘输入
+        processInput(window);
+
+
+
+        //我们可以通过调用glClear函数来清空屏幕的颜色缓冲，它接受一个缓冲位(Buffer Bit)来指定要清空的缓冲，可能的缓冲位有GL_COLOR_BUFFER_BIT，GL_DEPTH_BUFFER_BIT和GL_STENCIL_BUFFER_BIT。由于现在我们只关心颜色值，所以我们只清空颜色缓冲。
+        //注意，除了glClear之外，我们还调用了glClearColor来设置清空屏幕所用的颜色。当调用glClear函数，清除颜色缓冲之后，整个颜色缓冲都会被填充为glClearColor里所设置的颜色。在这里，我们将屏幕设置为了类似黑板的深蓝绿色。
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //glClearColor函数是一个状态设置函数，而glClear函数则是一个状态使用的函数，它使用了当前的状态来获取应该清除为的颜色。
+
+        // 渲染指令
+        
+        //这里我们会对opengl的缓冲区做文章 
+
+        // 检查并调用事件，交换缓冲，解释名词之交换缓冲：应用程序使用单缓冲绘图时可能会存在图像闪烁的问题。 这是因为生成的图像不是一下子被绘制出来的，而是按照从左到右，由上而下逐像素地绘制而成的。最终图像不是在瞬间显示给用户，而是通过一步一步生成的，这会导致渲染的结果很不真实。为了规避这些问题，我们应用双缓冲渲染窗口应用程序。前缓冲保存着最终输出的图像，它会在屏幕上显示；而所有的的渲染指令都会在后缓冲上绘制。当所有的渲染指令执行完毕后，我们交换(Swap)前缓冲和后缓冲，这样图像就立即呈显出来，之前提到的不真实感就消除了。
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    //释放资源
+    glfwTerminate();
+
+    return 0;
+}
+
+
+
+
+
+
+
+//回调函数
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+//处理键盘的输入
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
